dbce59c83f1b95046a4dadbf9aac2da2e2241f42
RAHUL <rahulbarnwalofficial@gmail.com>
2022-07-15 00:32:39 +0530

    Project completed

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const hbs = require('hbs');
+const port = process.env.PORT || 3000;
+
+// public static path 
+const static_path = path.join(__dirname, '../public');
+const template_path = path.join(__dirname, '../templates/views')
+const partials_path = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
+
+app.use(express.static(static_path));
+
+app.get('', (req, res) => {
+    res.render('index');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/weather', (req, res) => {
+    res.render('weather');
+})
+
+app.get('*', (req, res) => {
+    res.render('404page', {
+        errMsg: "Oops! page not found, Click Here to go back"
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Listening to the port no. ${port}`);
+})
